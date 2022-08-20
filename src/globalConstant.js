1786f3930747a981c7a3036b32ebb3828c6894f8
vino00619 <vinoth00619@gmail.com>
2022-08-20 13:08:46 +0530

    yhn

--- src/globalConstant.js
+++ src/globalConstant.js
@@ -1,6 +1,7 @@
 //export const API_URL = "http://localhost:8800";
 
-export const API_URL = "https://map-backend.herokuapp.com";
+// export const API_URL = "https://map-backend.herokuapp.com";
+export const API_URL = "https://mapprojectt.herokuapp.com";
 
 
 //    "react-map-gl": "^7.0.18",
