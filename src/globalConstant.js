f35a60b9356745f62a38afac61b7bc106392a8a1
vino00619 <vinoth00619@gmail.com>
2022-12-26 13:03:11 +0530

    vercel link updated

--- src/globalConstant.js
+++ src/globalConstant.js
@@ -1,7 +1,7 @@
 //export const API_URL = "http://localhost:8800";
 
 // export const API_URL = "https://map-backend.herokuapp.com";
-export const API_URL = "https://map-backend-96tm0v4w8-vino00619.vercel.app/";
+export const API_URL = "https://map-backend.vercel.app/";
 
 
 //    "react-map-gl": "^7.0.18",
