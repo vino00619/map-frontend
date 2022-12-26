3f728e2705391ea241ce66fcf79af25442eff865
vino00619 <vinoth00619@gmail.com>
2022-12-26 12:46:49 +0530

    vecel link updated in global constant

--- src/globalConstant.js
+++ src/globalConstant.js
@@ -1,7 +1,7 @@
 //export const API_URL = "http://localhost:8800";
 
 // export const API_URL = "https://map-backend.herokuapp.com";
-export const API_URL = "https://mapprojectt.herokuapp.com";
+export const API_URL = "https://map-backend-96tm0v4w8-vino00619.vercel.app/";
 
 
 //    "react-map-gl": "^7.0.18",
