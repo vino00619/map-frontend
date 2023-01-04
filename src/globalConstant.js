4f4d5d3e01258f6db9f25c6d40acbfa795330700
Vinoth Manoharan <39054777+vino00619@users.noreply.github.com>
2023-01-04 23:33:20 +0530

    Update globalConstant.js
    constant updated

--- src/globalConstant.js
+++ src/globalConstant.js
@@ -1,7 +1,7 @@
 //export const API_URL = "http://localhost:8800";
 
 // export const API_URL = "https://map-backend.herokuapp.com";
-export const API_URL = "https://map-backend.vercel.app/";
+export const API_URL = "https://map-backend01.vercel.app/";
 
 
 //    "react-map-gl": "^7.0.18",
